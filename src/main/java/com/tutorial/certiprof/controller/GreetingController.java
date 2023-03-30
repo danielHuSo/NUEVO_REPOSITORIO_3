910bfc03b7ec48833879587e46596fac8fb26bdb
unknown <danielhs101@gmail.com>
2023-03-29 18:36:14 -0600

    se agrega la v1 del proyecto java

---
+++ src/main/java/com/tutorial/certiprof/controller/GreetingController.java
@@ -0,0 +1,27 @@
+package com.tutorial.certiprof.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class GreetingController {
+
+	@RequestMapping(value = "/greeting", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
+	public String greeting(HttpServletRequest request, Model model) {
+		String firstName = request.getParameter("firstName");
+		String middleName = request.getParameter("middleName");
+		String lastName = request.getParameter("lastName");
+		String birthDate = request.getParameter("birthDate");
+
+		model.addAttribute("firstName", firstName);
+		model.addAttribute("middleName", middleName);
+		model.addAttribute("lastName", lastName);
+		model.addAttribute("birthDate", birthDate);
+		return "greeting";
+	}
+
+}
